4139a130e9a956ab486a2985666c18a1609cc2fa
andyadc <jjwadc@gmail.com>
2025-02-23 12:10:15 +0800

    context request factory

---
+++ kratos-context/src/main/java/com/andyadc/kratos/context/factory/RequestFactory.java
@@ -0,0 +1,149 @@
+package com.andyadc.kratos.context.factory;
+
+import com.andyadc.kratos.common.constants.Constants;
+import com.andyadc.kratos.common.constants.Protocol;
+import com.andyadc.kratos.common.enums.ResponseCode;
+import com.andyadc.kratos.common.exception.NullException;
+import com.andyadc.kratos.common.exception.UnmatchPathException;
+import com.andyadc.kratos.common.util.MatchUtils;
+import com.andyadc.kratos.context.attribute.AttributeKeyFactory;
+import com.andyadc.kratos.context.cache.ConfigCache;
+import com.andyadc.kratos.context.cache.ConfigCacheFactory;
+import com.andyadc.kratos.context.ctx.GatewayContext;
+import com.andyadc.kratos.context.ctx.RequestResponseContext;
+import com.andyadc.kratos.context.definition.ServiceDefinition;
+import com.andyadc.kratos.context.invoker.ServiceInvoker;
+import com.andyadc.kratos.context.request.DefaultHttpGatewayRequest;
+import com.andyadc.kratos.context.request.HttpGatewayRequest;
+import com.andyadc.kratos.context.rule.Rule;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpUtil;
+import org.apache.commons.lang3.StringUtils;
+
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 获取请求信息工厂类
+ */
+public class RequestFactory {
+
+    /**
+     * 获取网关上下文
+     */
+    public static GatewayContext getContext(FullHttpRequest fullHttpRequest, ChannelHandlerContext ctx) {
+        // 获取请求request对象数据
+        HttpGatewayRequest request = getRequest(fullHttpRequest, ctx);
+        // 获取服务定义信息
+        ServiceDefinition definition = getServiceDefinition(request);
+        // 如果路径匹配失败，则直接抛出异常
+        if (!MatchUtils.isMatch(definition.getPatternPath(), request.getPath())) {
+            throw new UnmatchPathException();
+        }
+
+        ServiceInvoker serviceInoker = getServiceInoker(request, definition);
+        String ruleId = serviceInoker.getRuleId();
+        ConfigCache cache = ConfigCacheFactory.getInstance();
+        Rule rule = cache.getRule(ruleId);
+        GatewayContext context = RequestResponseContext.getRequestResponseContext(definition.getProtocol(), ctx, HttpUtil.isKeepAlive(fullHttpRequest), request, rule);
+        context.setServerReceiveRequestTime(request.getBeginTime());
+        putContext(context, serviceInoker);
+        return context;
+    }
+
+    /**
+     * 获取网关上下文
+     */
+    private static HttpGatewayRequest getRequest(FullHttpRequest fullHttpRequest, ChannelHandlerContext ctx) {
+        HttpHeaders headers = fullHttpRequest.headers();
+        String uniqueId = headers.get(Constants.UNIQUE_ID);
+        if (StringUtils.isEmpty(uniqueId)) {
+            throw new NullException(ResponseCode.REQUEST_PARSE_ERROR_NO_UNIQUEID);
+        }
+        String host = headers.get(HttpHeaderNames.HOST);
+        HttpMethod method = fullHttpRequest.method();
+        String uri = fullHttpRequest.uri();
+        String clientIp = getClientIp(ctx, fullHttpRequest);
+        String contentType = HttpUtil.getMimeType(fullHttpRequest) == null ? null : HttpUtil.getMimeType(fullHttpRequest).toString();
+        Charset charset = HttpUtil.getCharset(fullHttpRequest, StandardCharsets.UTF_8);
+        return new DefaultHttpGatewayRequest(uniqueId, charset, clientIp, host, uri, method, contentType, headers, fullHttpRequest);
+    }
+
+    /**
+     * 获取客户端的ip地址
+     */
+    private static String getClientIp(ChannelHandlerContext ctx, FullHttpRequest request) {
+        String forwardedValue = request.headers().get(Constants.HTTP_FORWARD_SEPARATOR);
+
+        String clientIp = null;
+        if (StringUtils.isNotEmpty(forwardedValue)) {
+            List<String> values = Arrays.asList(forwardedValue.split(Constants.FORWARD_VALUE_SPLIT));
+            if (values.size() >= 1 && StringUtils.isNotBlank(values.get(0))) {
+                clientIp = values.get(0);
+            }
+        }
+        if (clientIp == null) {
+            InetSocketAddress inetSocketAddress = (InetSocketAddress) ctx.channel().remoteAddress();
+            clientIp = inetSocketAddress.getAddress().getHostAddress();
+        }
+        return clientIp;
+    }
+
+
+    /**
+     * 获取服务定义信息
+     */
+    public static ServiceDefinition getServiceDefinition(HttpGatewayRequest request) {
+        ServiceDefinition definition = ConfigCacheFactory.getInstance().getServiceDefinition(request.getUniqueId());
+        if (definition == null) {
+            throw new NullException(ResponseCode.SERVICE_DEFINITION_NOT_FOUND);
+        }
+        return definition;
+    }
+
+
+    /**
+     * 根据上下文和服务定义信息获取ServiceInvoker
+     */
+    private static ServiceInvoker getServiceInoker(HttpGatewayRequest context, ServiceDefinition definition) {
+        Map<String, ServiceInvoker> invokerMap = definition.getInvokerMap();
+        ServiceInvoker serviceInvoker = invokerMap.get(context.getPath());
+        if (serviceInvoker == null) {
+            throw new NullException(ResponseCode.SERVICE_INVOKER_NOT_FOUND);
+        }
+        return serviceInvoker;
+    }
+
+    /**
+     * 将上下文信息设置到Context
+     */
+    private static void putContext(GatewayContext context, ServiceInvoker invoker) {
+        String protocol = context.getProtocol();
+        if (Protocol.HTTP.equals(protocol)) {
+            context.putAttribute(AttributeKeyFactory.getHttpInvoker(), invoker);
+        } else if (Protocol.RPC.equals(protocol)) {
+            context.putAttribute(AttributeKeyFactory.getRpcInvoker(), invoker);
+        } else if (Protocol.WEBSOCKET.equals(protocol)) {
+            context.putAttribute(AttributeKeyFactory.getWebSocketInvoker(), invoker);
+        } else {
+            context.putAttribute(AttributeKeyFactory.getHttpInvoker(), invoker);
+        }
+
+//        switch (context.getProtocol()) {
+//            case Protocol.HTTP -> context.putAttribute(AttributeKeyFactory.getHttpInvoker(), invoker);
+//            case Protocol.RPC -> context.putAttribute(AttributeKeyFactory.getRpcInvoker(), invoker);
+//            case Protocol.WEBSOCKET -> context.putAttribute(AttributeKeyFactory.getWebSocketInvoker(), invoker);
+//            default -> context.putAttribute(AttributeKeyFactory.getHttpInvoker(), invoker);
+//        }
+
+    }
+
+}
