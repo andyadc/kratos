77674329f9ed492a4a63882be8bed3b1f6f67be1
andyadc <jjwadc@gmail.com>
2025-04-01 09:02:02 +0800

    filter

---
+++ kratos-filter/kratos-filter-api/src/main/java/com/andyadc/kratos/filter/chain/AbstractFilterChain.java
@@ -0,0 +1,22 @@
+package com.andyadc.kratos.filter.chain;
+
+import com.andyadc.kratos.filter.base.AbstractGatewayFilter;
+
+/**
+ * 抽象过滤器链
+ *
+ * @param <T>
+ */
+public abstract class AbstractFilterChain<T> extends AbstractGatewayFilter<T> {
+
+    /**
+     * 添加元素到元素头部
+     */
+    public abstract void addFirst(AbstractGatewayFilter<T> filter);
+
+    /**
+     * 添加元素到链路尾部
+     */
+    public abstract void addLast(AbstractGatewayFilter<T> filter);
+
+}
