8e4ca6a1b3042caed9760b4c934f18e4dad0f952
andyadc <jjwadc@gmail.com>
2025-04-13 19:06:45 +0800

    filter timeout

---
+++ kratos-filter/kratos-filter-timeout/src/main/java/com/andyadc/kratos/filter/timeout/TimeoutFilter.java
@@ -0,0 +1,45 @@
+package com.andyadc.kratos.filter.timeout;
+
+import com.andyadc.kratos.common.constants.Protocol;
+import com.andyadc.kratos.context.attribute.AttributeKeyFactory;
+import com.andyadc.kratos.context.config.FilterConfig;
+import com.andyadc.kratos.context.ctx.GatewayContext;
+import com.andyadc.kratos.context.ctx.RequestResponseContext;
+import com.andyadc.kratos.context.invoker.RpcServiceInvoker;
+import com.andyadc.kratos.context.invoker.WebSocketServiceInvoker;
+import com.andyadc.kratos.context.request.HttpGatewayRequest;
+import com.andyadc.kratos.filter.api.entry.AbstractEntryGatewayFilter;
+
+public class TimeoutFilter extends AbstractEntryGatewayFilter<FilterConfig> {
+
+    public TimeoutFilter() {
+        super(FilterConfig.class);
+    }
+
+    @Override
+    public void execute(GatewayContext context, Object... args) throws Exception {
+        try {
+            RequestResponseContext requestResponseContext = (RequestResponseContext) context;
+            String protocol = requestResponseContext.getProtocol();
+            switch (protocol) {
+                case Protocol.HTTP:
+                    HttpGatewayRequest request = requestResponseContext.getRequest();
+                    request.setRequestTimeout(filterConfig.getTimeout());
+                    break;
+                case Protocol.RPC:
+                    RpcServiceInvoker rpcServiceInvoker = (RpcServiceInvoker) requestResponseContext.getRequiredAttribute(AttributeKeyFactory.getRpcInvoker());
+                    rpcServiceInvoker.setTimeout(filterConfig.getTimeout());
+                    break;
+                case Protocol.WEBSOCKET:
+                    WebSocketServiceInvoker webSocketServiceInvoker = (WebSocketServiceInvoker) requestResponseContext.getRequiredAttribute(AttributeKeyFactory.getWebSocketInvoker());
+                    webSocketServiceInvoker.setTimeout(filterConfig.getTimeout());
+                    break;
+                default:
+                    break;
+            }
+        } finally {
+            super.nextFilter(context, args);
+        }
+    }
+
+}
