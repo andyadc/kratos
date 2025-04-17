89024e56846dbe81ca9911342a9ea30f2ff2c6b5
andyadc <jjwadc@gmail.com>
2025-04-17 09:06:41 +0800

    common filter websocket

---
+++ kratos-filter/kratos-filter-route-websocket/src/main/java/com/andyadc/kratos/filter/route/websocket/WebSocketFilter.java
@@ -0,0 +1,36 @@
+package com.andyadc.kratos.filter.route.websocket;
+
+import com.andyadc.kratos.common.constants.FilterConstants;
+import com.andyadc.kratos.common.enums.FilterType;
+import com.andyadc.kratos.context.config.FilterConfig;
+import com.andyadc.kratos.context.ctx.GatewayContext;
+import com.andyadc.kratos.filter.api.annotation.Filter;
+import com.andyadc.kratos.filter.api.entry.AbstractEntryGatewayFilter;
+import com.andyadc.kratos.spi.annotation.SPIClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * WebSocket过滤器
+ */
+@SPIClass
+@Filter(
+        id = FilterConstants.WEBSOCKET_FILTER_ID,
+        name = FilterConstants.WEBSOCKET_FILTER_NAME,
+        value = FilterType.ROUTE,
+        order = FilterConstants.WEBSOCKET_FILTER_ORDER
+)
+public class WebSocketFilter extends AbstractEntryGatewayFilter<FilterConfig> {
+
+    private final Logger logger = LoggerFactory.getLogger(WebSocketFilter.class);
+
+    public WebSocketFilter() {
+        super(FilterConfig.class);
+    }
+
+    @Override
+    public void execute(GatewayContext gatewayContext, Object... args) throws Exception {
+
+    }
+
+}
