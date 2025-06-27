1aa9581ebcd6aa05d4c5aa84d91ed9ccb95cd11f
andyadc <jjwadc@gmail.com>
2025-06-27 09:21:07 +0800

    processor api

---
+++ kratos-processor/kratos-processor-api/src/main/java/com/andyadc/kratos/processor/api/concurrent/thread/NamedThreadFactory.java
@@ -0,0 +1,36 @@
+package com.andyadc.kratos.processor.api.concurrent.thread;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class NamedThreadFactory implements ThreadFactory {
+
+    private final ThreadGroup group;
+    private final String namePrefix;
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
+    private boolean daemon;
+
+    public NamedThreadFactory(String name, boolean daemon) {
+        this(name);
+        this.daemon = daemon;
+    }
+
+    public NamedThreadFactory(String name) {
+        group = Thread.currentThread().getThreadGroup();
+        namePrefix = name + "-thread-";
+        daemon = Thread.currentThread().isDaemon();
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
+        t.setDaemon(daemon);
+
+        if (t.getPriority() != Thread.NORM_PRIORITY) {
+            t.setPriority(Thread.NORM_PRIORITY);
+        }
+
+        return t;
+    }
+
+}
