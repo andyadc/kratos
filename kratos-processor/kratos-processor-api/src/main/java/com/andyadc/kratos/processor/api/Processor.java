66031cfec8089be7da71afc1360fb3fb49621959
andyadc <jjwadc@gmail.com>
2025-07-19 10:48:24 +0800

    processor

--- kratos-processor/kratos-processor-api/src/main/java/com/andyadc/kratos/processor/api/Processor.java
+++ kratos-processor/kratos-processor-api/src/main/java/com/andyadc/kratos/processor/api/Processor.java
@@ -2,16 +2,18 @@ package com.andyadc.kratos.processor.api;
 
 import com.andyadc.kratos.context.config.GatewayConfig;
 import com.andyadc.kratos.context.request.HttpGatewayRequestWrapper;
+import com.andyadc.kratos.spi.annotation.SPI;
 
 /**
  * 处理器接口
  */
+@SPI
 public interface Processor {
 
     /**
      * 初始化处理器
      */
-    default void init(GatewayConfig gatewayConfig, Processor processor, boolean usedExecutorPool) {
+    default void init(GatewayConfig gatewayConfig, Processor processor) {
     }
 
     /**
