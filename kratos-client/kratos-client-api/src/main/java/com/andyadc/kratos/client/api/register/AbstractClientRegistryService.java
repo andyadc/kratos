91efe2203e22f0aa1cd5ac178022fcc4bb200fda
andyadc <jjwadc@gmail.com>
2025-08-08 09:01:07 +0800

    client api

---
+++ kratos-client/kratos-client-api/src/main/java/com/andyadc/kratos/client/api/register/AbstractClientRegistryService.java
@@ -0,0 +1,10 @@
+package com.andyadc.kratos.client.api.register;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class AbstractClientRegistryService {
+
+    private static final Logger logger = LoggerFactory.getLogger(AbstractClientRegistryService.class);
+
+}
---
+++ kratos-client/kratos-client-api/src/main/java/com/andyadc/kratos/client/api/scanner/GatewayClassScanner.java
@@ -0,0 +1,10 @@
+package com.andyadc.kratos.client.api.scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GatewayClassScanner {
+
+    private static final Logger logger = LoggerFactory.getLogger(GatewayClassScanner.class);
+
+}
